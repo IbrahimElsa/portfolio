b5817e0f7a354e042943b149d11d42505b908789
Rossthesloth900 <ibrahim.elsawalhi@outlook.com>
2024-03-01 19:40:36 -0500

    Add gsap library and animate navbar and modal

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+import { gsap } from 'gsap';
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Animate the navbar
+gsap.from(navbar, {duration: 1, y: -100, ease: 'power2.out'});
+
 // Get the modal
 var modal = document.getElementById("contactModal");
 
@@ -9,17 +17,17 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  gsap.to(modal, {autoAlpha: 1, scale: 1, duration: 1});
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  gsap.to(modal, {autoAlpha: 0, scale: 0.8, duration: 1});
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    gsap.to(modal, {autoAlpha: 0, scale: 0.8, duration: 1});
   }
 }
