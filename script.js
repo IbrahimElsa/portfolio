e1885f8bdd2e1c8cdbcb8635b16ee03f52f71bf3
Ibrahim Elsawalhi <ielsawalhi@usf.edu>
2024-12-06 18:24:35 -0500

    mobile formatting and added card colors on hover

--- script.js
+++ script.js
@@ -1,54 +1,16 @@
-
-var navbar = document.getElementById("navbar");
-
-var modal = document.getElementById("contactModal");
-
-var btn = document.getElementById("contactLink");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  gsap.to(modal, {autoAlpha: 1, scale: 1, duration: 1});
-}
-
-span.onclick = function() {
-  gsap.to(modal, {autoAlpha: 0, scale: 0.8, duration: 1});
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    gsap.to(modal, {autoAlpha: 0, scale: 0.8, duration: 1});
-  }
-}
-
-function App() {
-  const conf = {
-    nx: 40,
-    ny: 100,
-    cscale: chroma.scale(['red', '#B22222', '#7B3F00', '#404040', '#1C1C1C', '#000000']).mode('lch'),
-    darken: -1,
-    angle: Math.PI / 3,
-    timeCoef: 0.1
-  };
-
-  let renderer, scene, camera;
-  let width, height;
-  const { randFloat: rnd } = THREE.Math;
-
-  const uTime = { value: 0 }, uTimeCoef = { value: conf.timeCoef };
-  const polylines = [];
-
-  init();
-
-  function init() {
-    renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('canvas'), antialias: true });
-    camera = new THREE.PerspectiveCamera();
-
-    updateSize();
-    window.addEventListener('resize', updateSize, false);
-    document.body.addEventListener('click', initRandomScene);
-
-    initScene();
-    requestAnimationFrame(animate);
-  }
-}
+document.querySelectorAll('.project').forEach(project => {
+  const techIcons = project.querySelectorAll('i[class^="devicon-"]');
+  
+  project.addEventListener('mouseenter', () => {
+      techIcons.forEach(icon => {
+          const baseClass = icon.className.split(' ')[0];
+          icon.classList.add('colored');
+      });
+  });
+  
+  project.addEventListener('mouseleave', () => {
+      techIcons.forEach(icon => {
+          icon.classList.remove('colored');
+      });
+  });
+});
